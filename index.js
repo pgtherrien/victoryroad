b74a79ed6fd0c89b5f0fee4a7486e48362c423f3
Peter <peter@Peters-MacBook-Pro.local>
2019-06-04 19:28:01 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "client/built")));
+
+app.get("/api/version", (req, res) => {
+  var packageJSON = require("./package.json");
+  res.json(packageJSON.version);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.js"));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port);
+
+console.log(`Victory Road listening on ${port}`);
